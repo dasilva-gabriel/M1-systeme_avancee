e495bd1c8a05f982967eeb6d08c79fe048be9cc1
dasilvag <gabriel.dsilva@etu.univ-paris-diderot.fr>
2022-01-24 18:30:58 +0100

    ~End of TP02

--- TP02/exercice2.c
+++ TP02/exercice2.c
@@ -3,6 +3,8 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <dirent.h>
 
 #define BUFSIZE 124
 
@@ -21,6 +23,29 @@ int isRegularFile(const char *path)
     if (stat(path, &status) == -1 || (status.st_mode & S_IFMT) != S_IFREG)
     {
         perror("L'une des référence n'est pas un fichier.");
+        return -1;
+    }
+
+    close(file);
+
+    return 1;
+}
+
+int isDirectory(const char *path)
+{
+
+    int file = open(path, O_RDONLY);
+    if (file < 0)
+    {
+        perror("Erreur dans le stat...");
+        return -1;
+    }
+
+    struct stat status;
+
+    if (stat(path, &status) == -1 || (status.st_mode & S_IFMT) != S_IFDIR)
+    {
+        perror("L'une des référence n'est pas un répertoire.");
         return -1;
     }
 
@@ -81,7 +106,7 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
-    if (argv[1] != "-r")
+    if (strcmp(argv[1], "-r"))
     {
         if (isRegularFile(argv[2]) == 1 && isRegularFile(argv[3]) == 1)
         {
@@ -99,10 +124,25 @@ int main(int argc, char const *argv[])
                 printf("Oh... Oh... les fichiers ne sont pas identiques, ligne: %d\n", diffRes);
             }
         }
-    } else {
+    }
+    else
+    {
 
-        
+        if (isDirectory(argv[2]) == 1 && isDirectory(argv[3]))
+        {
 
+            DIR* dirA = opendir(argv[2]);
+            DIR* dirB = opendir(argv[3]);
+
+            struct dirent* reader;
+            while((reader = readdir(dirA)) != NULL) {
+
+                if(isRegularFile(reader->d_name)) {
+
+                }
+            }
+
+        }
     }
 
     return 0;
