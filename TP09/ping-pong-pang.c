56a0952e2639f23a750ffa89efdc8e62bc3f7471
dasilvag <gabriel.dsilva@etu.univ-paris-diderot.fr>
2022-03-31 09:35:06 +0200

    ~Change ping-pong-pang

--- TP09/ping-pong-pang.c
+++ TP09/ping-pong-pang.c
@@ -6,6 +6,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/*
+
+PARTIE 1
+
 int main(int argc, char const *argv[])
 {
     printf("ping\n");
@@ -15,5 +19,52 @@ int main(int argc, char const *argv[])
         wait(0);
         printf("pang\n");
     }
+    return 0;
+
+}*/
+
+void handler( int sig ){}
+
+int main(int argc, char const *argv[]) {
+    
+    int pid = fork();
+    if (pid == 0) {
+
+        printf("ping\n");
+
+        struct sigaction signal;
+        signal.sa_handler = handler;
+        sigfillset(&signal.sa_mask);
+        signal.sa_flags = 0;
+
+        if( sigaction(SIGUSR1, &signal, NULL) > 0) {
+            exit(1);
+        }
+            
+        kill(getppid(), SIGUSR1);
+        pause();
+
+        printf("pang\n");
+
+    } else {
+
+        struct sigaction signal;
+        signal.sa_handler = handler;
+        sigfillset(&signal.sa_mask);
+        signal.sa_flags = 0;
+
+        if( sigaction(SIGUSR1, &signal, NULL) > 0) {
+            exit(1);
+        }
+            
+        pause();
+
+        printf("pong\n");
+
+        kill(pid, SIGUSR1);
+
+    }
+    
+
     return 0;
 }
